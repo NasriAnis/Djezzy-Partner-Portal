776e504109c9e86e738c713c68bb3a1fcf389494
NasriAnis <nasrianis213@gmail.com>
2026-09-15 11:45:03 +0100

    ALLOWED_HOSTS all

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
